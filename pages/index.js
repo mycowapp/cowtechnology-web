2e489e3655e332a941370d4dda7b92373f8d2e71
mycowapp <92849668+mycowapp@users.noreply.github.com>
2025-02-28 14:18:35 +0100

    Update main context

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
 
           .flex-container {
           display: flex;
-          width: 100px;
+          max-width: 500px;
           flex-direction: column;
           background-color: rgb(217, 217, 217);
           text-align: center;
@@ -41,7 +41,7 @@ export default function Home() {
 
           .flex-container > div {
             background-color: #f1f1f1;
-            width: 100px;
+            width: 300px;
             margin: 10px;
             text-align: center;
             line-height: 75px;
