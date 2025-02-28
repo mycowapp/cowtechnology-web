71e17af0fe452212b051474f25281f492b7444df
mycowapp <92849668+mycowapp@users.noreply.github.com>
2025-02-28 15:23:14 +0100

    Update main context

--- pages/index.js
+++ pages/index.js
@@ -61,16 +61,16 @@ export default function Home() {
 
       <Header title="Cow Technology" />
       <main>
-        <h1>User Data Privacy Policy</h1>
-        <h2>Introduction</h2>
+        <h2>User Data Privacy Policy</h2>
+        <h3>Introduction</h3>
         <div>
           <p>This Privacy Policy describes how we, Cow Technology, collects, uses, and discloses information, and what choices you have with respect to the information when you use our mobile applications. Given our commitment to your privacy, our app has been designed to not require or store any personal data.</p>  
         </div>
 
-        <h2>Updates to this Policy</h2>
+        <h3>Updates to this Policy</h3>
         <p>We may update this Privacy Policy from time to time. We encourage you to review this Privacy Policy frequently to be informed of how we are protecting your information.</p>
 
-        <h2>Information We Collect and Use</h2>
+        <h3>Information We Collect and Use</h3>
         <p>Our application allows you to create, save, and delete data on your device. Here's how we handle your data:</p>
         <ul>
           <li><strong>Data Storage on Your Device:</strong> All data you create are stored directly on your device. We do not have access to any of the content you create.</li>
@@ -78,19 +78,19 @@ export default function Home() {
           <li><strong>Permissions:</strong> The app may request permission to access storage on your device to save or delete your data that is saved via our app. This permission is solely for the operation of the app and is not used for any other purpose.</li>
         </ul>
 
-        <h2>How We Use Your Information</h2>
+        <h3>How We Use Your Information</h3>
         <p>Since we do not collect personal data, there is no usage of your information outside of the direct functionality of the app—creating, saving, and deleting the data on your device.</p>
 
-        <h2>Data Sharing</h2>
+        <h3>Data Sharing</h3>
         <p>We do not collect, store, or share any personal information. Therefore, there is no data about you that could be shared with third parties.</p>
 
-        <h2>Security</h2>
+        <h3>Security</h3>
         <p>We are committed to ensuring the security of your data. Since all data is stored locally on your device, the security of your data is largely dependent on the security measures of your device.</p>
 
-        <h2>Your Rights</h2>
+        <h3>Your Rights</h3>
         <p>You have full control over your data, as it is stored on your device. You can create, modify, and delete data at any time. If you uninstall the app, all data stored by the app will also be deleted from your device.</p>
 
-        <h2>Contact Us</h2>
+        <h3>Contact Us</h3>
         <p>If you have any questions about this Privacy Policy, please contact us.</p>
       </main>
 
